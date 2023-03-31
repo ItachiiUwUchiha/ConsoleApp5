3e51443a021ddd4daa15d34c5fec5c10281a4d90
konradgd2005@gmail.com <konradgd2005@gmail.com>
2023-03-31 10:25:33 +0200

    GADERYPOLUKI

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//tekst do zaszyfrowania
+string clearText = "GADERYPOLUKI";
+//zmienna do przechowywania zaszyfrowanego tekstu
+string cypherText = "";
+//zamien G na A i zapisz do nowej zmiennej 
+cypherText = clearText.Replace('G', 'A');
+//wez juz czesciowo zaszyfrowany tekst i zamien A na G
+cypherText = clearText.Replace('A', 'G');
+//d->e, e->d
+cypherText = cypherText.Replace('D', 'E');
+cypherText = cypherText.Replace('E', 'D');
+//r->, e->d
+cypherText = cypherText.Replace('R', 'Y');
+cypherText = cypherText.Replace('Y', 'R');
+//p->o, o->p
+cypherText = cypherText.Replace('P', 'O');
+cypherText = cypherText.Replace('O', 'P');
+//l->u, o->p
+cypherText = cypherText.Replace('L', 'U');
+cypherText = cypherText.Replace('U', 'L');
+//k->i, i->k
+cypherText = cypherText.Replace('K', 'I');
+cypherText = cypherText.Replace('I', 'K');
+
+Console.WriteLine("Zaszyfrowany tekst: " + cypherText);
