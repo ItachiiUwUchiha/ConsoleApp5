7f5065220b44158483716e93c661ce78ba18ae88
konradgd2005@gmail.com <konradgd2005@gmail.com>
2023-03-31 10:34:14 +0200

    gaderypoluki2

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -1,32 +1,41 @@
-﻿//tekst do zaszyfrowania
+﻿string gaderypoluki(string clearText)
+{
+    //zmienna do przechowywania zaszyfrowanego tekstu
+    string cypherText = "";
+    //zamien G na A i zapisz do nowej zmiennej 
+    cypherText = clearText.Replace('G', '@');
+    //wez juz czesciowo zaszyfrowany tekst i zamien A na G
+    //uzyj znaku posredniego aby zapobiec dwukrotnej zamianie jednej litery
+    cypherText = clearText.Replace('A', 'G');
+    cypherText = clearText.Replace('@', 'A');
+    //d->e, e->d
+    cypherText = cypherText.Replace('D', '@');
+    cypherText = cypherText.Replace('E', 'D');
+    cypherText = cypherText.Replace('@', 'E');
+    //r->, e->d
+    cypherText = cypherText.Replace('R', '@');
+    cypherText = cypherText.Replace('Y', 'R');
+    cypherText = cypherText.Replace('@', 'Y');
+    //p->o, o->p
+    cypherText = cypherText.Replace('P', '@');
+    cypherText = cypherText.Replace('O', 'P');
+    cypherText = cypherText.Replace('@', 'O');
+    //l->u, o->p
+    cypherText = cypherText.Replace('L', '@');
+    cypherText = cypherText.Replace('U', 'L');
+    cypherText = cypherText.Replace('@', 'U');
+    //k->i, i->k
+    cypherText = cypherText.Replace('K', '@');
+    cypherText = cypherText.Replace('I', 'K');
+    cypherText = cypherText.Replace('@', 'I');
+
+    return cypherText;
+}
+
+
+//tekst do zaszyfrowania
 string clearText = "GADERYPOLUKI";
-//zmienna do przechowywania zaszyfrowanego tekstu
-string cypherText = "";
-//zamien G na A i zapisz do nowej zmiennej 
-cypherText = clearText.Replace('G', '@');
-//wez juz czesciowo zaszyfrowany tekst i zamien A na G
-//uzyj znaku posredniego aby zapobiec dwukrotnej zamianie jednej litery
-cypherText = clearText.Replace('A', 'G');
-cypherText = clearText.Replace('@', 'A');
-//d->e, e->d
-cypherText = cypherText.Replace('D', '@');
-cypherText = cypherText.Replace('E', 'D');
-cypherText = cypherText.Replace('@', 'E');
-//r->, e->d
-cypherText = cypherText.Replace('R', '@');
-cypherText = cypherText.Replace('Y', 'R');
-cypherText = cypherText.Replace('@', 'Y');
-//p->o, o->p
-cypherText = cypherText.Replace('P', '@');
-cypherText = cypherText.Replace('O', 'P');
-cypherText = cypherText.Replace('@', 'O');
-//l->u, o->p
-cypherText = cypherText.Replace('L', '@');
-cypherText = cypherText.Replace('U', 'L');
-cypherText = cypherText.Replace('@', 'U');
-//k->i, i->k
-cypherText = cypherText.Replace('K', '@');
-cypherText = cypherText.Replace('I', 'K');
-cypherText = cypherText.Replace('@', 'I');
+//zaszyfruj uzywajac funkcji
+string cypherText = gaderypoluki(clearText);
 
 Console.WriteLine("Zaszyfrowany tekst: " + cypherText);
